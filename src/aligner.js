d672e57a8af546a2dca7c2dcb83e5dfdaa9f71b5
root <account-login@users.noreply.github.com>
2020-04-15 19:11:51 +0800

    Allow align page to left, right, or center.

--- src/aligner.js
+++ src/aligner.js
@@ -1,63 +1,56 @@
 
-var html = document.getElementsByTagName('html')[0];
-
-
-var curPaddingNum = parseInt(localStorage.getItem('centerPagesPadding') || 0);
-
-if (curPaddingNum > 0) {
-  adjustPadding(curPaddingNum, 0)
-}
-
-function adjustPadding(curPaddingNum, diff) {
-
-  var contentCont = document.documentElement;
-  var newPaddingNum = parseInt(curPaddingNum + diff)
-
-  if (newPaddingNum < 0) {
-    newPaddingNum = 0
+function update(padding, align) {
+  var newPadding = padding + parseInt(localStorage.getItem('centerPagesPadding') || 0);
+  if (newPadding < 0) {
+    newPadding = 0;
   }
+  var newAlign = align || localStorage.getItem('centerPagesAlign') || 'center';
 
-  var newPadding = newPaddingNum + 'px';
+  var left = 0;
+  var right = 0;
+  if (newAlign === 'left') {
+    right = newPadding * 2;
+  } else if (newAlign === 'right') {
+    left = newPadding * 2;
+  } else {
+    left = newPadding;
+    right = newPadding;
+  }
+  var contentCont = document.documentElement;
+  contentCont.style.borderLeft = left.toFixed() + 'px solid transparent';
+  contentCont.style.borderRight = right.toFixed() + 'px solid transparent';
 
-  contentCont.style.borderLeft = newPadding + ' solid transparent';
-  contentCont.style.borderRight = newPadding + ' solid transparent';
-
-  localStorage.setItem('centerPagesPadding', newPaddingNum);
-
-
-}
-
-function increasePadding() {
-  /**/
-  var curPaddingNum = parseInt(localStorage.getItem('centerPagesPadding') || 0);
-  adjustPadding(curPaddingNum, 25)
   // This is needed because of a bug with Chrome not re-rendering css immediately.
   document.getElementsByTagName('body')[0].focus();
 
-  return curPaddingNum + 25
+  localStorage.setItem('centerPagesPadding', newPadding);
+  localStorage.setItem('centerPagesAlign', newAlign);
 }
 
-
-function decreasePadding() {
-  /**/
-  var curPaddingNum = parseInt(localStorage.getItem('centerPagesPadding') || 0);
-
-  adjustPadding(curPaddingNum, -25)
-  // This is needed because of a bug with Chrome not re-rendering css immediately.
-  document.getElementsByTagName('body')[0].focus();
+function query() {
+  return {
+    padding: localStorage.getItem('centerPagesPadding'),
+    align: localStorage.getItem('centerPagesAlign'),
+  }
 }
 
+if (localStorage.getItem('centerPagesPadding')) {
+  update(0, '');
+}
 
 chrome.runtime.onMessage.addListener(
   /*
     Listen to extension menu actions.
-    */
-  function (request) {
-    if (request.changePadding === "increase") {
-      return increasePadding();
+   */
+  function (request, sender, sendResponse) {
+    console.log('s', sender);
+    if (request.query) {
+      sendResponse(query());
+    } else if (request.changePadding === "increase") {
+      update(+25, '');
     } else if (request.changePadding === "decrease") {
-      decreasePadding();
+      update(-25, '');
+    } else if (request.changeAlign) {
+      update(0, request.changeAlign);
     }
   });
-
-
--- src/popup.js
+++ src/popup.js
@@ -1,25 +1,59 @@
 //
 //
 document.addEventListener('DOMContentLoaded', function () {
-
-
   var increaseButton = document.getElementById('centerPagesIncrease');
   var decreaseButton = document.getElementById('centerPagesDecrease');
 
   increaseButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { changePadding: "increase" }, function (response) {
-        console.log(response);
+      chrome.tabs.sendMessage(tabs[0].id, { changePadding: "increase" }, function () {
+        if (chrome.runtime.lastError) {   // This check will suppress the "Unchecked lastError value ..." msg.
+          return
+        }
       });
     });
   }, false);
 
   decreaseButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { changePadding: "decrease" }, function (response) {
-        // console.log(response.increase_completed);
+      chrome.tabs.sendMessage(tabs[0].id, { changePadding: "decrease" }, function () {
+        if (chrome.runtime.lastError) {   // This check will suppress the "Unchecked lastError value ..." msg.
+          return
+        }
       });
     });
   }, false);
+
+  // check radio box
+  var radioBoxes = document.querySelectorAll("input[name='centerPagesAlign']");
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs.length === 0) {
+      return;
+    }
+    chrome.tabs.sendMessage(tabs[0].id, { query: true }, function (response) {
+      if (chrome.runtime.lastError) {   // This check will suppress the "Unchecked lastError value ..." msg.
+        return
+      }
+      for (var i = 0; i < radioBoxes.length; ++i) {
+        if (radioBoxes[i].value === response.align) {
+          radioBoxes[i].checked = true;
+          break;
+        }
+      }
+    });
+  });
+
+  // radio box click event
+  for (var i = 0; i < radioBoxes.length; ++i) {
+    radioBoxes[i].addEventListener('change', function (event) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { changeAlign: event.target.value }, function () {
+          if (chrome.runtime.lastError) {   // This check will suppress the "Unchecked lastError value ..." msg.
+            return
+          }
+        });
+      });
+    }, false);
+  }
 
 }, false);
