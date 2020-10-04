a0de2eb258092e3cb980b0a3fd3b4ab0f5051e14
feluxe <felix@meyerwolters.de>
2020-10-04 16:57:58 +0200

    1.2.1

--- src/aligner.js
+++ src/aligner.js
@@ -1,41 +1,42 @@
 
 function update(padding, align) {
-  var newPadding = padding + parseInt(localStorage.getItem('centerPagesPadding') || 0);
+  var newPadding =
+    padding + parseInt(localStorage.getItem("centerPagesPadding") || 0);
   if (newPadding < 0) {
     newPadding = 0;
   }
-  var newAlign = align || localStorage.getItem('centerPagesAlign') || 'center';
+  var newAlign = align || localStorage.getItem("centerPagesAlign") || "center";
 
   var left = 0;
   var right = 0;
-  if (newAlign === 'left') {
+  if (newAlign === "left") {
     right = newPadding * 2;
-  } else if (newAlign === 'right') {
+  } else if (newAlign === "right") {
     left = newPadding * 2;
   } else {
     left = newPadding;
     right = newPadding;
   }
   var contentCont = document.documentElement;
-  contentCont.style.borderLeft = left.toFixed() + 'px solid transparent';
-  contentCont.style.borderRight = right.toFixed() + 'px solid transparent';
+  contentCont.style.borderLeft = left.toFixed() + "px solid transparent";
+  contentCont.style.borderRight = right.toFixed() + "px solid transparent";
 
   // This is needed because of a bug with Chrome not re-rendering css immediately.
-  document.getElementsByTagName('body')[0].focus();
+  document.getElementsByTagName("body")[0].focus();
 
-  localStorage.setItem('centerPagesPadding', newPadding);
-  localStorage.setItem('centerPagesAlign', newAlign);
+  localStorage.setItem("centerPagesPadding", newPadding);
+  localStorage.setItem("centerPagesAlign", newAlign);
 }
 
 function query() {
   return {
-    padding: localStorage.getItem('centerPagesPadding'),
-    align: localStorage.getItem('centerPagesAlign'),
-  }
+    padding: localStorage.getItem("centerPagesPadding"),
+    align: localStorage.getItem("centerPagesAlign"),
+  };
 }
 
-if (localStorage.getItem('centerPagesPadding')) {
-  update(0, '');
+if (localStorage.getItem("centerPagesPadding")) {
+  update(0, "");
 }
 
 chrome.runtime.onMessage.addListener(
@@ -46,10 +47,11 @@ chrome.runtime.onMessage.addListener(
     if (request.query) {
       sendResponse(query());
     } else if (request.changePadding === "increase") {
-      update(+25, '');
+      update(+25, "");
     } else if (request.changePadding === "decrease") {
-      update(-25, '');
+      update(-25, "");
     } else if (request.changeAlign) {
       update(0, request.changeAlign);
     }
-  });
+  }
+);
